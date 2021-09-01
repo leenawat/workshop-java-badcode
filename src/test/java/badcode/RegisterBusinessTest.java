f0debec0858cd0e185c770543910d9772bb081e7
Leenawat Papahom <leenawat.pa@gmail.com>
2021-09-01 14:53:39 +0700

    return id when save success

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -181,4 +181,29 @@ public class RegisterBusinessTest {
 
     }
 
+    @Test
+    @DisplayName("return id when save success")
+    public void case12() {
+        // Arrange
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("my-firstname");
+        speaker.setLastName("my-lastname");
+        speaker.setEmail("my-email@gmail.com");
+
+        SpeakerRepository speakerRepository = new SpeakerRepository() {
+            @Override
+            public Integer saveSpeaker(Speaker speaker) {
+                return 1;
+            }
+        };
+
+        // Act
+        Integer id = registerBusiness.register(speakerRepository, speaker);
+
+        // Assert
+        assertEquals(1, id);
+    }
+
 }
